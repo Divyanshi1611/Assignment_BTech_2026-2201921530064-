4022ee08c2a7f5a9d847ab14b57cf2ec8f76a4f2
Divyanshi1611 <sharmadivyanshi622@gmail.com>
2025-01-26 23:57:29 +0530

    modified

---
+++ DSA/reversePairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+        int revPair = 0;
+        int n = nums.size();
+        vector<int> sortedNums;
+
+        for (int i = n - 1; i >= 0; i--) {
+            auto it = lower_bound(sortedNums.begin(), sortedNums.end(), nums[i] / 2.0);
+            revPair += it - sortedNums.begin();
+            sortedNums.insert(upper_bound(sortedNums.begin(), sortedNums.end(), nums[i]), nums[i]);
+        }
+
+        return revPair;
+    }
+};
