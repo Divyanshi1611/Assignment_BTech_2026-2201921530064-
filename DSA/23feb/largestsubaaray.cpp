754223567cb30f0ddcad3ac04b221e3cdc49e5ff
Divyanshi1611 <sharmadivyanshi622@gmail.com>
2025-02-23 23:39:23 +0530

    modified

---
+++ DSA/23feb/largestsubaaray.cpp
@@ -0,0 +1,53 @@
+//https://www.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1?category%5B%5D=Hash&company%5B%5D=Amazon&page=1&query=category%5B%5DHashcompany%5B%5DAmazonpage1company%5B%5DAmazoncategory%5B%5DHash&utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=largest-subarray-with-0-sum
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*You are required to complete this function*/
+
+class Solution {
+  public:
+    int maxLen(vector<int>& arr) {
+        unordered_map<int, int> prefixSumIndex;
+        int maxLength = 0, sum = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            sum += arr[i];
+            if (sum == 0) maxLength = i + 1;
+            if (prefixSumIndex.find(sum) != prefixSumIndex.end()) 
+                maxLength = max(maxLength, i - prefixSumIndex[sum]);
+            else prefixSumIndex[sum] = i;
+        }
+        return maxLength;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+    while (t--) {
+        int n;
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.maxLen(a) << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
